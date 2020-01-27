ad57f3b56407c82da491b299c801f3c90a92c8b3
JulianoAcunia <Juliano.acunia@hotmail.com>
2020-01-27 15:19:16 -0300

    Db url and console log

--- server.js
+++ server.js
@@ -4,3 +4,8 @@ const mongoose = require ('mongoose')
 const app = express()
 const port = 5000
 
+const mongoDBURL = require('./dbconfig/connectionstring.config')
+
+app.listen( port, () => {
+    console.log(`Example app listening on port ${port}!`)
+})
