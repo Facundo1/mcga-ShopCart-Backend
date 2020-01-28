91f95f9051e3b0a5993d17e0f076292d2aa42f37
JulianoAcunia <Juliano.acunia@hotmail.com>
2020-01-28 11:03:55 -0300

    User model

---
+++ app/models/users.js
@@ -0,0 +1,33 @@
+const mongoose = require ('mongoose')
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema ({
+    id: {
+        type: Number,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        default: 0
+    }
+})
+
+const User = mongoose.model('users',UserSchema)
+
+module.exports = User
