ec70a5622ca4bd85abf35af6aeb9bcd837f2304b
Facundo Loberse <facundosa123@gmail.com>
2020-02-03 11:29:27 -0300

    model changes

--- app/models/products.js
+++ app/models/products.js
@@ -2,9 +2,6 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const ProductSchema = new Schema({
-    _id: {
-        type: Number
-    },
     name: {
         type: String
     },
