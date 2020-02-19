a88f911b2abbedfd7f5e8a04db0fb186cc2f34a7
JulianoAcunia <Juliano.acunia@hotmail.com>
2020-02-19 13:49:01 -0300

    Product model correction

--- app/models/products.js
+++ app/models/products.js
@@ -2,14 +2,23 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const ProductSchema = new Schema({
-    name: {
+    _id: {
+        type: Number
+    },
+    title: {
         type: String
     },
     description: {
         type: String
     },
+    availableSize: {
+        type: String
+    },
     price: {
         type: Number
+    },
+    isFreeShipping: {
+        type: Boolean
     }
 },{versionKey: false})
 
